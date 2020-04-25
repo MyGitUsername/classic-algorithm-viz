dcddd50ca0294e4763968b083c3e951210835e14
M K <michaelkauf@gmail.com>
2020-04-25 13:57:13 -0700

    Adding selection sort and timeline control button shell components

---
+++ src/components/SelectionSort.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-btn @click="refreshList()">New Data</v-btn>
+        <v-btn @click="swapPairs = bubbleSort(list)">Sort</v-btn>
+      </v-col>
+    </v-row>
+    <BarChartVisualization
+      :list="list"
+      :swapPairs="swapPairs"
+      />
+  </v-container>
+</template>
+
+<script>
+import BarChartVisualization from '@/components/BarChartVisualization.vue';
+import listGenerator from '@/mixins/listGenerator.js';
+
+export default {
+  name: 'SelectionSort',
+  components: {
+    BarChartVisualization
+  },
+  mixins: [listGenerator],
+  data () {
+    return {
+      listSize: 0,
+      swapPairs: []
+    }
+  },
+  methods: {
+    /* Returns a an array of swaps (index pairs) that will
+     * sort this list
+     */
+    selectionSort (arr) {
+      let steps = [];
+      for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+          if (arr[j + 1] < arr[j]) {
+            steps.push([j, j + 1])
+            let tmp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = tmp;
+          }
+        }
+      }
+      return steps;
+    }
+  },
+  mounted () {
+    this.refreshList();
+  }
+}
+</script>
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/components/TimelineControlButtons.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-row>
+    <v-btn @click='tl.tweenTo(tl.previousLabel())' >
+      <img src="@/assets/step-backward-solid.svg" height=20 width=20>
+    </v-btn>
+    <v-btn @click='tl.play()' >
+      <img src="@/assets/play-solid.svg" height=20 width=20>
+    </v-btn>
+    <v-btn @click='tl.tweenTo(tl.nextLabel())'>
+      <img src="@/assets/pause-solid.svg" height=20 width=20>
+    </v-btn>
+    <v-btn @click='tl.tweenTo(tl.nextLabel())' >
+      <img src="@/assets/step-forward-solid.svg" height=20 width=20>
+    </v-btn>
+  </v-row>
+</template>
+
+<script>
+/*
+ * The svg icons used in this componenet were provided by font-awesome
+ * under the Creative * Commons license (https://fontawesome.com/license).
+ * No changes have been made to the original svg.
+ */
+
+
+export default {
+  name: 'TimelineControlButtons',
+  data () {
+    return {
+      //
+    }
+  },
+  props: {
+    tl: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    play () {
+      console.log('you hit play')
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
